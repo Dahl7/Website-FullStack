2d5454a90fd87efbafeef84fffa7d553f9d1663f
Dingdong <=>
2025-04-15 17:40:43 +0200

    Cleaned up

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import LoginPage from "./pages/Loginpage";
 import RestaurantSelector from "./pages/Restaurantselector";
-import Menupage from "./pages/Menupage";
+import Menupage from "./pages/menupages/Menupage";
 import MenuSectionsPage from "./pages/menupages/MenuSectionPage";
 import SectionItemsPage from "./pages/menupages/SectionItemsPage";
-import BreakfastPage from "./pages/menupages/BreakfastPage"
 import FetchTest from "./FetchTest";
 import PostTest from "./PostTest";
 import Orderpage from "./pages/Orderpage";
@@ -22,7 +21,6 @@ function App() {
         <Route path="/menu" element={<Menupage />} />
         <Route path="/menu/sections" element={<MenuSectionsPage />} />
         <Route path="/menu/sectionItems" element={<SectionItemsPage />} />
-        <Route path ="/menu/breakfast" element ={<BreakfastPage/>}/>
         <Route path="/FetchTest" element={<FetchTest />} /> 
         <Route path="/PostTest" element={<PostTest />} /> 
         <Route path="/orders" element={<Orderpage />} />
