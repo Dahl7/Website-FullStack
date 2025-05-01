31d631d7379dcd256cf5367ea9011914bc04fa95
Lars Nissen <larsnissen@larss-mbp.students.clients.local>
2025-05-01 14:07:26 +0200

    fix

--- src/pages/Paymentpages/Successpage.js
+++ src/pages/Paymentpages/Successpage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import './PaymentSuccess.css'
+import './Successpage.css'
 
 const PaymentSuccess = () => {
   const [searchParams] = useSearchParams();
